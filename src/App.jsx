d814910739082d326dacf65615769b1328d1a812
Aran-tm <armandortmontes@gmail.com>
2024-03-08 10:51:24 -0500

    adding first styles for header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
+import Header from './components/Header'
 
 function App() {
   
 
   return (
-    <>
-      
-    </>
+    <div className=''>
+          
+        {/* Este es Header importado en React */}
+        <Header/>
+    </div>
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+// componente HEADER
+
+export const Header = () => {
+ return (
+    <>
+      {/* Este es un comentario en React */}
+      <div>Header</div>
+    </>
+ );
+};
+
+export default Header
