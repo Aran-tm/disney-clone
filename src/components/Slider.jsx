8d4c68169257658e0807593d81e11b16afe862ae
Aran-tm <armandortmontes@gmail.com>
2024-03-09 13:06:38 -0500

    adding arrows direction

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -19,13 +19,21 @@ function Slider() {
   }
 
   return (
-      //  Con overflow-x-auto me pone un scrollbar 
-      <div className='flex overflow-x-auto w-full px-16 py-4 scrollbar-none'>
-        {movieList.map((item) => (
-            <img key={item.id} src={IMAGE_BASE_URL + item.backdrop_path} alt={item.title}
-            className='min-w-full h-[410px] object-cover object-left-top mr-5 rounded-lg'/>
-        ))}
-    </div>
+      //  Con overflow-x-auto me pone un scrollbar
+      // scrollbar-none oculta mi barra de desplazamiento horizontal para este caso
+      <div>
+          
+          {/* Flechas para  */}
+          <svg className='text-[40px] absolute mx-5 mt-[220px] cursor-pointer' xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 64 64"><path fill="currentColor" d="M32 2C15.432 2 2 15.432 2 32c0 16.568 13.432 30 30 30s30-13.432 30-30C62 15.432 48.568 2 32 2m17 35.428H30.307V48L15 32l15.307-16v11.143H49z"/></svg>
+          <svg className='text-[40px] absolute mx-5 mt-[220px] right-0' xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 64 64"><path fill="currentColor" d="M32 2C15.432 2 2 15.432 2 32c0 16.568 13.432 30 30 30s30-13.432 30-30C62 15.432 48.568 2 32 2m1.693 46V37.428H15V27.143h18.693V16L49 32z" /></svg>
+          
+        <div className='flex overflow-x-auto w-full px-16 py-4 scrollbar-none'>
+            {movieList.map((item) => (
+                <img key={item.id} src={IMAGE_BASE_URL + item.backdrop_path} alt={item.title}
+                className='min-w-full h-[410px] object-cover object-left-top mr-5 rounded-lg'/>
+            ))}
+        </div>
+      </div>
   )
 }
 
